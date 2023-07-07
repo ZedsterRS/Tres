133b1d1e9e259258d1827df1e5aac3946f56b646
Zedsters <108711210+ZedsterRS@users.noreply.github.com>
2023-07-07 14:28:08 -0400

    Add files via upload

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -2,13 +2,16 @@ module.exports = {
   name: "messageCreate",
   run: (client, messageCreate) => {
     const prefix = process.env.PREFIX;
+    const prefix_2 = process.env.PREFIX_2;
 
-    if(!messageCreate.content.startsWith(prefix)) return;
+    if(!messageCreate.content.startsWith(prefix)){
+      if(!messageCreate.content.startsWith(prefix_2)) return
+    };
 
     const args = messageCreate.content.slice(prefix.length).trim().split(/ +/g);
     const argCommand = args.shift().toLowerCase();
     let cmd = client.prefix_Commands.find((c) => c.name === argCommand);
 		
-    if(cmd) {cmd.run(client, messageCreate, args, prefix)}
+    if(cmd) {cmd.run(client, messageCreate, args, prefix)};
   }
 }
